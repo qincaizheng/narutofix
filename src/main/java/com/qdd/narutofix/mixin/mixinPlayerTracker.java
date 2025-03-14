9e28505a8749931e483450d9ae360400f86d740f
caizheng.qin <dhdxqcz@gmail.com>
2025-03-14 10:31:19 +0800

    修复忍者经验命令的mixin

--- src/main/java/com/qdd/narutofix/mixin/mixinPlayerTracker.java
+++ src/main/java/com/qdd/narutofix/mixin/mixinPlayerTracker.java
@@ -1,7 +1,11 @@
 package com.qdd.narutofix.mixin;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.translation.I18n;
 import net.narutomod.PlayerTracker;
+import net.narutomod.procedure.ProcedureSync;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,14 +17,26 @@ import static net.narutomod.NarutomodModVariables.BATTLEXP;
 @Mixin(PlayerTracker.class)
 public abstract class mixinPlayerTracker {
 
-    @Shadow
+    @Shadow(remap = false)
     public static double getBattleXp(EntityPlayer player) {
-        return 0;
+        return player.getEntityData().getDouble(BATTLEXP);
     }
 
-    @Inject(method = "addBattleXp(Lnet/minecraft/entity/player/EntityPlayer;DZ)V",at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;setDouble(Ljava/lang/String;D)V",shift = At.Shift.AFTER),remap = false)
+    @Shadow(remap = false)
+    private static void sendBattleXPToTracking(EntityPlayerMP player) {
+        ProcedureSync.EntityNBTTag.sendToTracking(player, BATTLEXP, getBattleXp(player));
+    }
+
+    @Inject(method = "addBattleXp(Lnet/minecraft/entity/player/EntityPlayer;DZ)V",at = @At(value = "HEAD"),remap = false, cancellable = true)
     private static void addBattleXp(EntityPlayer entity, double xp, boolean sendMessage, CallbackInfo ci) {
         entity.getEntityData().setDouble(BATTLEXP, getBattleXp(entity) + xp);
+        if (entity instanceof EntityPlayerMP) {
+            sendBattleXPToTracking((EntityPlayerMP)entity);
+            if (sendMessage) {
+                entity.sendStatusMessage(new TextComponentString(I18n.translateToLocal("chattext.ninjaexperience") + String.format("%.1f", getBattleXp(entity))), true);
+            }
+        }
+        ci.cancel();
     }
 }
 
