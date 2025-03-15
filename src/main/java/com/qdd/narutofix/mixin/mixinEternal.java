68908ac91e67c680001a52d614105a64d8433471
qincaizheng <dhdxqcz@gmail.com>
2025-03-15 16:03:43 +0800

    修复永恒眼吞其他眼睛的特性

--- src/main/java/com/qdd/narutofix/mixin/mixinEternal.java
+++ src/main/java/com/qdd/narutofix/mixin/mixinEternal.java
@@ -1,4 +1,22 @@
 package com.qdd.narutofix.mixin;
 
-public class mixinEternal {
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+
+@Mixin(targets ="net.narutomod.item.ItemMangekyoSharinganEternal$1" )
+public abstract class mixinEternal extends net.narutomod.item.ItemDojutsu.Base{
+
+    public mixinEternal(ArmorMaterial material) {
+        super(material);
+    }
+
+    public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) {
+        super.onUpdate(itemstack, world, entity, par4, par5);
+    }
+
 }
+
